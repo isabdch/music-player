3c1952939ef09e8a9011f2692d613a7745795d9b
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2022-01-13 09:29:40 -0300

    update

---
+++ script/script.js
@@ -0,0 +1,16 @@
+// get HTML elements 
+// info
+let nowPlaying = document.getElementById("now-playing");
+let songImg = document.getElementById("img-song");
+let songName = document.getElementById("song-name");
+let artistName = document.getElementById("artist-name");
+// buttons 
+let previousSong = document.getElementById("song-previous");
+let playPause = document.getElementById("pause-play");
+let nextSong = document.getElementById("song-next");
+// song-slider
+let currentTime = document.getElementsByClassName("current-time")[0];
+let songSlider = document.getElementsByClassName("song-slider")[0];
+let totalDuration = document.getElementsByClassName("total-duration")[0];
+// volume-slider
+let volumeSlider = document.getElementsByClassName("volume-slider")[0];
