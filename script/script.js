22af69ab463e02c63e9c32747dfd775c370f43d1
Isabelle Brandão <91973420+isabdch@users.noreply.github.com>
2022-01-14 11:35:44 -0300

    update

--- script/script.js
+++ script/script.js
@@ -9,7 +9,7 @@ let songName = document.getElementById("song-name");
 let artistName = document.getElementById("artist-name");
 
 // buttons
-let restart = document.getElementById("restart");
+let randomSong = document.getElementById("random-song");
 let previousSong = document.getElementById("song-previous");
 let playPause = document.getElementById("pause-play");
 let nextSong = document.getElementById("song-next");
@@ -25,6 +25,7 @@ let volumeSlider = document.getElementById("volume-slider");
 
 // specify global variables values
 let isPlaying = false;
+let randomSg = false;
 let trackIndex = 0;
 let updateTimer;
 
@@ -41,7 +42,6 @@ let trackList = [
     path: "./songs/FKJ - Ylang Ylang (Slowed-Reverb).mp3",
     artistnamecolor: "#65c995",
     darkcolor: "#7EDAAA",
-    backgroundColor: "#A1FFCE",
     backgroundColorGradient: "linear-gradient(to right, #FAFFD1, #A1FFCE)",
     github: "./images/github.png",
   },
@@ -53,8 +53,8 @@ let trackList = [
     path: "./songs/Ark Patrol - Let Go.mp3",
     artistnamecolor: "#c95038",
     darkcolor: "#dd664e",
-    backgroundColorGradient: "linear-gradient(to right, #faeae2, #f6cab7, #efaa90, #e7896d, #dd664e)",
-    github: "./images/github1.png"
+    backgroundColorGradient: "linear-gradient(to right, #faeae2, #dd664e)",
+    github: "./images/github1.png",
   },
   {
     name: "Love Unlimited",
@@ -62,10 +62,10 @@ let trackList = [
     image: "./images/love-unlimited.png",
     link: "https://open.spotify.com/artist/2TWKl87Jnmd5MApKEpVCxA",
     path: "./songs/Blackbird Blackbird - Love Unlimited.mp3",
-    artistnamecolor: "#389bd1",
+    artistnamecolor: "#1b86bf",
     darkcolor: "#389bd1",
-    backgroundColorGradient: "linear-gradient(to right, #cce1f2, #b2d6f1, #96cbf0, #75c0ee, #4bb5ed)",
-    github: "./images/github2.png"
+    backgroundColorGradient: "linear-gradient(to right, #dce8f2, #4bb5ed)",
+    github: "./images/github2.png",
   },
   {
     name: "love, death, distraction",
@@ -73,10 +73,10 @@ let trackList = [
     image: "./images/love-death-distraction.png",
     link: "https://open.spotify.com/artist/1t20wYnTiAT0Bs7H1hv9Wt",
     path: "./songs/EDEN - love, death, distraction.mp3",
-    artistnamecolor: "",
-    darkcolor: "",
-    backgroundColorGradient: "",
-    github: ""
+    artistnamecolor: "#7db3ae",
+    darkcolor: "#8fc7c2",
+    backgroundColorGradient: "linear-gradient(to right, #fff0dd, #97d4ee)",
+    github: "./images/github3.png",
   },
   {
     name: "Poison Tree",
@@ -84,10 +84,10 @@ let trackList = [
     image: "./images/poison-tree.png",
     link: "https://open.spotify.com/artist/31uyAcnY0kjjKKIQZMKX4i",
     path: "./songs/Grouper - Poison Tree.mp3",
-    artistnamecolor: "",
-    darkcolor: "",
-    backgroundColorGradient: "",
-    github: ""
+    artistnamecolor: "#000000",
+    darkcolor: "#363832",
+    backgroundColorGradient: "linear-gradient(to left, #6d6027, #d3cbb8)",
+    github: "./images/github4.png",
   },
   {
     name: "Comfort Chain",
@@ -95,10 +95,11 @@ let trackList = [
     image: "./images/comfort-chain.png",
     link: "https://open.spotify.com/artist/3ctnkEZGtVBTxS7IMin8nC",
     path: "./songs/Instupendo - Comfort Chain.mp3",
-    artistnamecolor: "",
-    darkcolor: "",
-    backgroundColorGradient: "",
-    github: ""
+    artistnamecolor: "#966bb0",
+    darkcolor: "#ab6fd1",
+    backgroundColorGradient:
+      "linear-gradient(to left, #a770ef, #cf8bf3, #fdb99b)",
+    github: "./images/github5.png",
   },
   {
     name: "Gravity",
@@ -106,10 +107,10 @@ let trackList = [
     image: "./images/gravity.png",
     link: "https://open.spotify.com/artist/1t20wYnTiAT0Bs7H1hv9Wt",
     path: "./songs/EDEN - Gravity.mp3",
-    artistnamecolor: "",
-    darkcolor: "",
-    backgroundColorGradient: "",
-    github: ""
+    artistnamecolor: "#313157",
+    darkcolor: "#4b4b6b",
+    backgroundColorGradient: "linear-gradient(to right, #eee5de, #25425b)",
+    github: "./images/github6.png",
   },
   {
     name: "Forever",
@@ -117,10 +118,11 @@ let trackList = [
     image: "./images/forever.png",
     link: "https://open.spotify.com/artist/2feDdbD5araYcm6JhFHHw7",
     path: "./songs/Labrinth – Forever.mp3",
-    artistnamecolor: "",
-    darkcolor: "",
-    backgroundColorGradient: "",
-    github: ""
+    artistnamecolor: "#779aa3",
+    darkcolor: "#97b5bd",
+    backgroundColorGradient:
+      "linear-gradient(to left, #cc95c0, #dbd4b4, #7aa1d2)",
+    github: "./images/github7.png",
   },
   {
     name: "Goodbye",
@@ -128,10 +130,10 @@ let trackList = [
     image: "./images/goodbye.png",
     link: "https://open.spotify.com/artist/1se3w7gpZkwcJYgHSPDoQ8",
     path: "./songs/Ramsey - Goodbye.mp3",
-    artistnamecolor: "",
-    darkcolor: "",
-    backgroundColorGradient: "",
-    github: ""
+    artistnamecolor: "#45352b",
+    darkcolor: "#59473c",
+    backgroundColorGradient: "linear-gradient(to left, #394a92, #fdbb2d)",
+    github: "./images/github8.png",
   },
   {
     name: "Yellow Box",
@@ -139,16 +141,16 @@ let trackList = [
     image: "./images/yellow-box.png",
     link: "https://open.spotify.com/artist/77SW9BnxLY8rJ0RciFqkHh",
     path: "./songs/The Neighbourhood - Yellow Box.mp3",
-    artistnamecolor: "",
-    darkcolor: "",
-    backgroundColorGradient: "",
-    github: ""
+    artistnamecolor: "#000000",
+    darkcolor: "#595959",
+    backgroundColorGradient: "linear-gradient(to left, #d0d1c5, #f2f2f2)",
+    github: "./images/github4.png",
   },
 ];
 //
 
 // add events to the elements
-restart.addEventListener("click", goToStart);
+randomSong.addEventListener("click", randomS);
 previousSong.addEventListener("click", prevTrack);
 playPause.addEventListener("click", playPauseTrack);
 nextSong.addEventListener("click", nextTrack);
@@ -157,18 +159,30 @@ songSlider.addEventListener("change", seekTo);
 volumeSlider.addEventListener("change", setVolume);
 
 // functions
-function goToStart() {
-  currTrack.currentTime = 0;
+function randomS() {
+  if (randomSg == false) {
+    randomSg = true;
+    randomSong.classList.replace("fad", "fas");
+  } else {
+    randomSg = false;
+    randomSong.classList.replace("fas", "fad");
+  }
 }
 //
+
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 function songLoop() {
   if (currTrack.loop == false) {
     currTrack.setAttribute("loop", true);
-    repeatSong.style.color = "#000000";
+    repeatSong.classList.replace("fad", "fas");
+    repeatSong.classList.replace("fa-repeat", "fa-repeat-1");
   } else {
     currTrack.removeAttribute("loop");
-    repeatSong.style.color = "#000000c0";
+    repeatSong.classList.replace("fas", "fad");
+    repeatSong.classList.replace("fa-repeat-1", "fa-repeat");
   }
 }
 
@@ -178,34 +192,85 @@ function loadTrack(trackIndex) {
   clearInterval(updateTimer);
   resetValues();
 
-  // load a new track
-  currTrack.src = trackList[trackIndex].path;
-  currTrack.load();
+  let rN = randomNumber(0, 9);
 
-  //update details of the track and page appearance
-  body.style.background = trackList[trackIndex].backgroundColorGradient;
-  songImg.style.boxShadow = "10px 8px 0px" + trackList[trackIndex].darkcolor;
-  songImg.src = trackList[trackIndex].image;
-  gb.src = trackList[trackIndex].github;
-  songSlider.style.setProperty("--track-background", trackList[trackIndex].darkcolor);
-  volumeSlider.style.setProperty("--track-background", trackList[trackIndex].darkcolor);
-  songName.textContent = trackList[trackIndex].name;
-  artistName.textContent = trackList[trackIndex].artist;
-  artistName.addEventListener("mouseover", function () {
-    artistName.style.color = trackList[trackIndex].artistnamecolor;
-  });
-  artistName.addEventListener("mouseout", function () {
-    artistName.style.color = "#000000c0";
-  });
-  nowPlaying.textContent =
-    "Now playing " + (trackIndex + 1) + " of " + trackList.length;
-  artistName.setAttribute("href", trackList[trackIndex].link);
+  if (randomSg == false) {
+    // load a new track
+    currTrack.src = trackList[trackIndex].path;
+    currTrack.load();
 
-  // set an interval of 1000 milliseconds (1 second) for updating the seek slider
-  updateTimer = setInterval(seekUpdate, 1000);
+    //update details of the track and page appearance
+    body.style.background = trackList[trackIndex].backgroundColorGradient;
+    songImg.style.boxShadow =
+      "10px 8px 0px" + trackList[trackIndex].artistnamecolor;
+    songImg.style.border = "3px solid" + trackList[trackIndex].darkcolor;
+    songImg.src = trackList[trackIndex].image;
+    gb.src = trackList[trackIndex].github;
+    songSlider.style.setProperty(
+      "--track-background",
+      trackList[trackIndex].darkcolor
+    );
+    volumeSlider.style.setProperty(
+      "--track-background",
+      trackList[trackIndex].darkcolor
+    );
+    songName.textContent = trackList[trackIndex].name;
+    artistName.textContent = trackList[trackIndex].artist;
+    artistName.addEventListener("mouseover", function () {
+      artistName.style.color = trackList[trackIndex].artistnamecolor;
+    });
+    artistName.addEventListener("mouseout", function () {
+      artistName.style.color = "#000000c0";
+    });
+    nowPlaying.textContent =
+      "Now playing " + (trackIndex + 1) + " of " + trackList.length;
+    artistName.setAttribute("href", trackList[trackIndex].link);
 
-  // move to the next track if the current finishes playing using ended event
-  currTrack.addEventListener("ended", nextTrack);
+    // set an interval of 1000 milliseconds (1 second) for updating the seek slider
+    updateTimer = setInterval(seekUpdate, 1000);
+
+    // move to the next track if the current finishes playing using ended event
+    currTrack.addEventListener("ended", nextTrack);
+  } else {
+    // load a new track
+    currTrack.src = trackList[rN].path;
+    currTrack.load();
+
+    //update details of the track and page appearance
+    body.style.background =
+      trackList[rN].backgroundColorGradient;
+    songImg.style.boxShadow =
+      "10px 8px 0px" + trackList[rN].artistnamecolor;
+    songImg.style.border =
+      "3px solid" + trackList[rN].darkcolor;
+    songImg.src = trackList[rN].image;
+    gb.src = trackList[rN].github;
+    songSlider.style.setProperty(
+      "--track-background",
+      trackList[rN].darkcolor
+    );
+    volumeSlider.style.setProperty(
+      "--track-background",
+      trackList[rN].darkcolor
+    );
+    songName.textContent = trackList[rN].name;
+    artistName.textContent = trackList[rN].artist;
+    artistName.addEventListener("mouseover", function () {
+      artistName.style.color = trackList[rN].artistnamecolor;
+    });
+    artistName.addEventListener("mouseout", function () {
+      artistName.style.color = "#000000c0";
+    });
+    nowPlaying.textContent =
+      "Now playing " + (rN + 1) + " of " + trackList.length;
+    artistName.setAttribute("href", trackList[rN].link);
+
+    // set an interval of 1000 milliseconds (1 second) for updating the seek slider
+    updateTimer = setInterval(seekUpdate, 1000);
+
+    // move to the next track if the current finishes playing using ended event
+    currTrack.addEventListener("ended", nextTrack);
+  }
 }
 //
 
@@ -326,4 +391,3 @@ function seekUpdate() {
   }
 }
 //
-
